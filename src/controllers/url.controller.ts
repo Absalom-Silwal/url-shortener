f66d472729565a6333337236a3fc8b36d1c05eb2
absalom <absalomsilwal93@gmail.com>
2026-01-13 17:36:10 +1100

    feat: snowflake ID generation and Base62 encoding helpers

--- src/controllers/url.controller.ts
+++ src/controllers/url.controller.ts
@@ -1,15 +1,19 @@
 import Url from "../models/url.model.ts";
 import bs58 from "bs58";
 import {Request,Response} from "express"
+import { createSnowflake,Base62 } from "../helpers/helpers.ts";
 
 // shorten Url
 export const shortUrl = async (req:Request, res:Response) => {
   try {
     const {url} = req.body;
-    const dataToEncode = Buffer.from(url, 'utf8');
-    const shorten_url = bs58.encode(dataToEncode);
-    const urlDb = await Url.findOneAndUpdate({short_code:shorten_url},{long_url:url},{ new: true, upsert: true });
-    res.status(201).json(urlDb);
+    //use snowflake technique for unique ID generation
+    const snowflake = createSnowflake(10);
+    const uniId = snowflake().toString();
+    const encodedId = Base62(BigInt(uniId));
+    console.log(encodedId);
+    //const urlDb = await Url.findOneAndUpdate({short_code:shorten_url},{long_url:url},{ new: true, upsert: true });
+    res.status(201).json({uniId:encodedId});
   } catch (error: unknown) {
     if (error instanceof Error){
       res.status(400).json({ error: error.message });
---
+++ src/helpers/helpers.ts
@@ -0,0 +1,43 @@
+export const createSnowflake = (workerId: number) => {
+  const EPOCH = 1577836800000n; // Jan 1 2020
+  let lastTimestamp = -1n;
+  let sequence = 0n;
+
+  return () => {
+    let now = BigInt(Date.now());
+
+    if (now === lastTimestamp) {
+      sequence = (sequence + 1n) & 0xFFFn; // 12 bits
+
+      // checking if generated sequence more than 4096.if generate sequence more than 4096, it will 
+      // wait until next millisecond to generate the sequence
+      if (sequence === 0n) {
+        while (now <= lastTimestamp) {
+          now = BigInt(Date.now());
+        }
+      }
+    } else {
+      sequence = 0n;
+    }
+
+    lastTimestamp = now;
+
+    const timestampPart = (now - EPOCH) << 22n;
+    const workerPart = (BigInt(workerId) & 0x3FFn) << 12n;
+    const sequencePart = sequence;
+
+    return timestampPart | workerPart | sequencePart;
+  };
+};
+
+export const Base62 = (uId:bigint)=>{
+    const Base62 = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+    let new_encoded_str = ''
+    const base = 62n
+    while(uId>0){
+        const rem = uId % base
+        new_encoded_str = Base62[Number(rem)] + new_encoded_str //prepending
+        uId = uId / base;
+    }
+    return new_encoded_str;
+}
